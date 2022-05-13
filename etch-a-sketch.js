5ed8c607d5dfe5898cdb04907e8020b0976a5b82
joshuaobare <jobare.obare@gmail.com>
2022-05-13 21:48:35 +0300

    sketchpad configured to change colour

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,8 +4,10 @@ const container=document.querySelector('#container')
 for (let x=0;x < 256; x++) {
     
     this["grid"+x] = document.createElement('div')  // creates a variable that differentiates itself from the next based on the iteration
-    this["grid"+x].classList.add('grid')
+    this["grid"+x].classList.add('grid')            // could've been an array lol
     container.appendChild(this["grid"+x])
-    
+    this["grid"+x].addEventListener('mouseover', () => {
+        this["grid"+x].classList.add('grid2')
+    })
 }
 
